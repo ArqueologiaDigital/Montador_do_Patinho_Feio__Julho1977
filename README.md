a37d39a0bab1aa8145a2056f79e6c4cadf5b2f9f
Felipe Corrêa da Silva Sanches <fsanches@metamaquina.com.br>
2015-11-24 16:38:35 -0200

    O professor João José Neto achou uma cópia da página 5.18

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
 # Montador_do_Patinho_Feio__Julho1977
 Escaneamento do documento "Montador do Patinho Feio (Julho 1977)"
-
-## observações
-
-A página 18 do capítulo 5 não foi escaneada por que está faltando na cópia do documento que temos acesso.
-Talvez alguém ainda tenha uma outra cópia desse documento sem essa página faltando ?
--- lowres/make_pdf.sh
+++ lowres/make_pdf.sh
@@ -2,8 +2,15 @@ echo "Generating lowres pages:"
 for f in ../*.tiff
 do
     echo "Converting $f ..."
-    convert $f -resize 800 $f.lowres.jpg
+    convert $f -resize 600x $f.lowres.jpg
 done
+
+# Essa página não constava no material e foi escaneada individualmente
+# pelo professor João José Neto a partir de outra cópia incompleta
+# do documento.
+# Eu jurava que o parametro do -resize deveria ser 600x mas por algum motivo
+# estranho, só acertei o tamanho com esses 2500x que ainda não consigo entender:
+convert ../5-18\ assembler\ pf.jpg -rotate 180 -resize 2500x ../C05.18.lowres.jpg
 
 echo "And now generating the PDF..."
 convert ../capa*.lowres.jpg ../I*.lowres.jpg ../C*.lowres.jpg ../A*.lowres.jpg PatinhoFeio_lowres.pdf
